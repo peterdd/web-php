50fff06e83b0a6b4d007f5b1207911ab3849a880
peterdd <ddpm@liscovius.de>
2021-09-25 02:58:13 +0200

    Update zh.php

--- releases/8.0/zh.php
+++ releases/8.0/zh.php
@@ -359,7 +359,7 @@ array_chunk([], -1); // ValueError: array_chunk(): Argument #2 ($length) must be
     关于 JIT 对 PHP 8 性能的贡献
   </h3>
   <p>
-    <img src="/images/php8/scheme.svg" width="900" alt="Just-In-Time compilation">
+    <img src="images/php8/scheme.svg" width="900" alt="Just-In-Time compilation">
   </p>
 
   <div class="php8-columns">
