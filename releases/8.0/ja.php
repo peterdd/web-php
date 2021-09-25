22d63f82ecacd3b562258b489f7d7b4cdb33ec93
peterdd <ddpm@liscovius.de>
2021-09-25 02:56:38 +0200

    Update ja.php

--- releases/8.0/ja.php
+++ releases/8.0/ja.php
@@ -371,7 +371,7 @@ array_chunk([], -1); // ValueError: array_chunk(): Argument #2 ($length) must be
     -->
   </h3>
   <p>
-    <img src="/images/php8/scheme.svg" width="900" alt="Just-In-Time compilation">
+    <img src="images/php8/scheme.svg" width="900" alt="Just-In-Time compilation">
   </p>
 
   <div class="php8-columns">
