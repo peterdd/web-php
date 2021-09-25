69cc46e04a45b036c55b0d5c946f020f80e58367
peterdd <ddpm@liscovius.de>
2021-09-25 02:57:19 +0200

    Update pt_BR.php

--- releases/8.0/pt_BR.php
+++ releases/8.0/pt_BR.php
@@ -370,7 +370,7 @@ array_chunk([], -1); // ValueError: array_chunk(): Argument #2 ($length) must be
           Relative JIT contribution to PHP 8 performance
         </h3>
         <p>
-            <img src="/images/php8/scheme.svg" width="900" alt="Just-In-Time compilation">
+            <img src="images/php8/scheme.svg" width="900" alt="Just-In-Time compilation">
         </p>
 
         <div class="php8-columns">
